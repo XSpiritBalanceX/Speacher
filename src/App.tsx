d206e634f2e60f681b86e71938dedf6a47400305
Helen <lenagreidon@mail.ru>
2023-09-10 13:40:30 +0300

    added images cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,77 @@
+import { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import aceOfHeart from "./images/aceOfHeart.jpg";
+import queenOfSpades from "./images/queenOfSpades.jpg";
+import sixOfClub from "./images/sixOfClubs.jpg";
+import kingOfTambourine from "./images/kingOfTambourine.jpg";
+
 const App = () => {
-  return <div></div>;
+  const [images, setImages] = useState("");
+  const queenPronansiation = ["Дама пик", "Пиковая дама"];
+  const sixPronansiation = ["Шестерка треф", "Шестёрка треф"];
+  const kingPronansiation = ["Король бубен", "Бубновый король"];
+
+  const commands = [
+    {
+      command: "Туз червей",
+      callback: () => setImages(aceOfHeart),
+      matchInterim: true,
+    },
+    {
+      command: queenPronansiation,
+      callback: () => setImages(queenOfSpades),
+      matchInterim: true,
+    },
+    {
+      command: sixPronansiation,
+      callback: () => setImages(sixOfClub),
+      matchInterim: true,
+    },
+    {
+      command: kingPronansiation,
+      callback: () => setImages(kingOfTambourine),
+      matchInterim: true,
+    },
+  ];
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+
+  useEffect(() => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Ups, your browser is not supported!");
+    }
+  }, []);
+
+  const startListening = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
+  const stopListening = () => {
+    SpeechRecognition.stopListening();
+    setImages("");
+    resetTranscript();
+  };
+
+  return (
+    <div>
+      <div>
+        <p>You: {transcript ? transcript : ""}</p>
+      </div>
+      <button onClick={startListening}>Start</button>
+      <button onClick={stopListening}>Stop</button>
+      <div>
+        {images && (
+          <img
+            src={images}
+            alt="card"
+            style={{ width: "20em", height: "30em" }}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default App;
